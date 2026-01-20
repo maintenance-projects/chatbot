b083ad358c82647c0ed6d3cc1960f48e75668508
hyunjunny <hjkim@ultari.co.kr>
2026-01-20 11:40:12 +0900

    [서버에서 템플릿리스트 내려주기] 1. 페이지 처음 접속 시 model 로 templateList 내려줌. [{"key":"A001","fileName":"template.hwpx","name":"결재보고","sort":"000001"},{"key":"A002","fileName":"template2.hwpx","name":"공지사항","sort":"000002"}]
    2. 위 데이터로 양식선택 화면 구성 하면 됨.
    3. 양식 선택 후 전송시 templateKey 에 key 값을 추가 해주면 됨.

--- src/main/java/kr/co/ultari/chatbot/generate/controller/GenerateController.java
+++ src/main/java/kr/co/ultari/chatbot/generate/controller/GenerateController.java
@@ -1,6 +1,8 @@
 package kr.co.ultari.chatbot.generate.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -12,8 +14,24 @@ public class GenerateController {
 
     @RequestMapping("/{key}")
     public String login(Model model, @PathVariable("key") String sessionId) {
+        JSONArray templateList = new JSONArray();
+        JSONObject templateObject = new JSONObject();
+
+        templateList.put(createTemplate("A001", "template.hwpx", "결재보고","000001"));
+        templateList.put(createTemplate("A002", "template2.hwpx", "공지사항","000002"));
+
         model.addAttribute("sessionId",sessionId);
+        model.addAttribute("templateList", templateList);
         return "dialog";
+    }
+
+    protected JSONObject createTemplate(String key, String fileName, String name, String sort) {
+        JSONObject json = new JSONObject();
+        json.put("key",key);
+        json.put("fileName",fileName);
+        json.put("name",name);
+        json.put("sort",sort);
+        return json;
     }
 
     /*@RequestMapping("/dialog")
